a35cebf07919082d23f1125ee6d49018186051cd
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-02 18:58:47 +0000

    Passed.2 - Added print class with heading printed on header call

---
+++ lib/print.rb
@@ -0,0 +1,7 @@
+class Print
+
+  def header
+    puts "The Students of Try Hard\n----------\n"
+  end
+
+end
--- spec/print_spec.rb
+++ spec/print_spec.rb
@@ -5,10 +5,11 @@ describe Print do
 
   describe 'header' do 
     it 'prints Header text' do
-      msg = "The Student of Try Hard\n----------\n"
+      msg = "The Students of Try Hard\n----------\n"
       expect { subject.header }.to output(msg).to_stdout
     end
   end
+end
 
 
 
