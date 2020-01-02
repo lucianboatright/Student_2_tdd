676c3fcec6c51c9f57b82a3cc6c3ceac653df91f
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-02 18:39:09 +0000

    Test.1 - Test student name/cohort saved as key,value

---
+++ spec/student_spec.rb
@@ -0,0 +1,13 @@
+require 'student'
+
+describe Student do
+
+  let(:new_student) { Student.new("Nic West", "November")}
+    it "store 1st arrangement passed :name key " do
+      expect(new_student.student[:name]).to eq("Nic West")
+    end
+
+    it "store 2nd arrgument passed as :cohort key" do
+      expect(new_student.student[:cohort]).to eq(:November)
+    end
+end
